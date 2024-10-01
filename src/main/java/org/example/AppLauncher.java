6f153347943520f73b1a6686d598e8f42cb8a7df
ruslana <ruslana.prus@ukr.net>
2024-10-01 21:15:08 +0200

    image server

--- src/main/java/org/example/AppLauncher.java
+++ src/main/java/org/example/AppLauncher.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 public class AppLauncher {
 
     public static void main(String[] args) throws IOException {
-        HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
-        downloader.downloadStatusImage(200);
-
         ImageWebServer.startServer();
     }
 }
--- src/main/java/org/example/ImageWebServer.java
+++ src/main/java/org/example/ImageWebServer.java
@@ -1,28 +1,122 @@
 package org.example;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.SimpleFileServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.FileHandler;
 
 public class ImageWebServer {
+    private static final Logger logger = LoggerFactory.getLogger(ImageWebServer.class);
+    public static final int MAX_CONNECTIONS = 100;
 
     public static void startServer() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", exchange -> {
-            Path pagePath = Paths.get("src/main/resources/web/index.html");
-            byte[] response = Files.readAllBytes(pagePath);
-
-            exchange.sendResponseHeaders(200, response.length);
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(response);
-            }
-        });
+        server.createContext("/", new StatusHandler());
+        server.createContext("/images", new AssetHandler("src/main/resources/web/images"));
+        ExecutorService executor = Executors.newFixedThreadPool(MAX_CONNECTIONS);
+        server.setExecutor(executor);
         server.start();
-        System.out.println("Server started at http://localhost:8080");
+        logger.info("Server started at http://localhost:8080");
+    }
+
+    static class AssetHandler implements HttpHandler {
+        private final String baseDirectory;
+
+        AssetHandler(String baseDirectory) {
+            this.baseDirectory = baseDirectory;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            logger.info("access images");
+            String filePath = baseDirectory + exchange.getRequestURI().getPath().replace("/images", "");
+            logger.info("filePath {}", filePath);
+            File file = new File(filePath);
+            if(file.exists() && !file.isDirectory()) {
+                exchange.sendResponseHeaders(200, file.length());
+                try(OutputStream os = exchange.getResponseBody();
+                    FileInputStream fis = new FileInputStream(file)) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = fis.read(buffer)) != -1) {
+                        os.write(buffer, 0, bytesRead);
+                    }
+                }
+            } else {
+                exchange.sendResponseHeaders(404, -1);
+            }
+        }
+    }
+
+    static class StatusHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
+            String path = exchange.getRequestURI().getPath();
+            String statusCode = path.substring(path.lastIndexOf('/') + 1);
+            downloader.downloadStatusImage(Integer.parseInt(statusCode));
+
+            String response = """
+                    <!DOCTYPE html>
+                         <html lang="en">
+                         <head>
+                             <meta charset="UTF-8">
+                             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                             <title>Status Image</title>
+                             <style>
+                                 body {
+                                     font-family: Arial, sans-serif;
+                                     text-align: center;
+                                     padding-top: 50px;
+                                 }
+                                 img {
+                                     max-width: 100%;
+                                     height: auto;
+                                     border: 1px solid #ddd;
+                                     border-radius: 4px;
+                                     padding: 5px;
+                                     width: 300px;
+                                 }
+                                 h1 {
+                                     color: #333;
+                                 }
+                             </style>
+                         </head>
+                         <body>
+                         <h1>HTTP Status Image</h1>
+                         <p>The image below shows the HTTP status code you requested:</p>
+                    
+                         <!-- Display the image -->
+                         <img src="./images/""" + statusCode +
+                    """
+                            .jpg" alt=""" + statusCode +
+                    """
+                            >
+                            <p>Image fetched from <a href="https://http.cat">https://http.cat</a>.</p>
+                            </body>
+                            </html>
+                            """;
+
+            byte[] responseBytes = response.getBytes();
+            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+            exchange.getResponseHeaders().set("Content-Length", String.valueOf(responseBytes.length));
+            exchange.sendResponseHeaders(200, responseBytes.length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(responseBytes);
+            }
+        }
     }
 }
