e072476f99eb8dcd0705d9bc43ddb915de50db73
ruslana <ruslana.prus@ukr.net>
2024-10-03 17:17:05 +0200

    start page

--- src/main/java/org/example/server/StatusHandler.java
+++ src/main/java/org/example/server/StatusHandler.java
@@ -19,24 +19,77 @@ public class StatusHandler implements RequestHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String path = exchange.getRequestURI().getPath();
+
+        if (path.equals("/") || path.equals("/index.html")) {
+            String response = generateDefaultHtmlResponse("Use /[status_code] to fetch an image.");
+            sendHtmlResponse(exchange, response);
+            return;
+        }
+
         String statusCode = path.substring(path.lastIndexOf('/') + 1);
 
         try {
             int code = Integer.parseInt(statusCode);
             imageManager.getImage(code);
             String response = generateHtmlResponse(statusCode);
-
-            byte[] responseBytes = response.getBytes();
-            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-            exchange.getResponseHeaders().set("Content-Length", String.valueOf(responseBytes.length));
-            exchange.sendResponseHeaders(200, responseBytes.length);
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(responseBytes);
-            }
+            sendHtmlResponse(exchange, response);
         } catch (NumberFormatException e) {
-            logger.error("Invalid status code: {}", statusCode, e);
-            exchange.sendResponseHeaders(400, -1);
+            logger.error("Invalid status code: {}", statusCode, e.getMessage());
+            String response = generateDefaultHtmlResponse("Invalid status code: " + statusCode + ". Please enter a valid number");
+            sendHtmlResponse(exchange, response);
         }
+    }
+
+    private static void sendHtmlResponse(HttpExchange exchange, String response) throws IOException {
+        byte[] responseBytes = response.getBytes();
+        exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+        exchange.getResponseHeaders().set("Content-Length", String.valueOf(responseBytes.length));
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(responseBytes);
+        }
+    }
+
+    private String generateDefaultHtmlResponse(String message) {
+        return """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Status Image</title>
+                    <style>
+                        @font-face {
+                                font-family: 'CustomFont';
+                                src: url('src/main/resources/fonts/Cattie-Regular.ttf') format('truetype');
+                            }
+                        body {
+                            font-family: 'CustomFont', Arial, sans-serif;
+                            text-align: center;
+                            padding-top: 50px;
+                        }
+                        img {
+                            max-width: 100%;
+                            height: auto;
+                            border: 1px solid #ddd;
+                            border-radius: 4px;
+                            padding: 5px;
+                            width: 300px;
+                        }
+                        h1 {
+                            color: #333;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>HTTP Status Image</h1>
+                    <p>""" + message +
+                """
+                        </p>
+                        </body>
+                        </html>
+                        """;
     }
 
     private String generateHtmlResponse(String statusCode) {
@@ -49,11 +102,15 @@ public class StatusHandler implements RequestHandler {
                          <meta name="viewport" content="width=device-width, initial-scale=1.0">
                          <title>Status Image</title>
                          <style>
-                             body {
-                                 font-family: Arial, sans-serif;
-                                 text-align: center;
-                                 padding-top: 50px;
-                             }
+                            @font-face {
+                                font-family: 'CustomFont';
+                                src: url('src/main/resources/fonts/Cattie-Regular.ttf') format('truetype');
+                            }
+                            body {
+                                font-family: 'CustomFont', Arial, sans-serif;
+                                text-align: center;
+                                padding-top: 50px;
+                            }
                              img {
                                  max-width: 100%;
                                  height: auto;
@@ -70,14 +127,14 @@ public class StatusHandler implements RequestHandler {
                      <body>
                      <h1>HTTP Status Image</h1>
                      <p>The image below shows the HTTP status code you requested:</p>
-                
+
                      <!-- Display the image -->
                      <img src="./images/""" + statusCode +
                 """
                         .jpg" alt=""" + statusCode +
                 """
                         >
-                        <p>Image fetched from <a href="https://http.cat">https://http.cat</a>.</p>
+                        <p>Images are fetched from <a href="https://http.cat">https://http.cat</a>.</p>
                         </body>
                         </html>
                         """;
